f4339843ccda81e887e91f76b48ee5dca5527150
KazNeco <tominaga@pmc.co.jp>
2026-05-26 12:06:12 +0900

    update

--- push_to_github.ps1
+++ push_to_github.ps1
@@ -1,26 +1,27 @@
 Set-Location $PSScriptRoot
 
 $git = "C:\Users\tominaga\AppData\Local\GitHubDesktop\app-3.5.10\resources\app\git\cmd\git.exe"
+$token = "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"
+$remote = "https://$token@github.com/tominaga-hash/kakeibo.git"
 
-# Windowsの認証ダイアログを無効化
-$env:GIT_ASKPASS = "echo"
+$env:GCM_INTERACTIVE = "never"
 $env:GIT_TERMINAL_PROMPT = "0"
-& $git config credential.helper ""
 
 if (-not (Test-Path ".git")) {
     Write-Host "First time setup..."
     & $git init
     & $git branch -M main
-    & $git remote add origin "https://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@github.com/tominaga-hash/kakeibo.git"
+    & $git remote add origin $remote
     & $git config user.email "tominaga@pmc.co.jp"
     & $git config user.name "KazNeco"
-    & $git fetch origin
+    & $git -c credential.helper="" fetch origin
     & $git reset --soft origin/main
     Write-Host "Setup done"
 }
 
 & $git config user.email "tominaga@pmc.co.jp"
 & $git config user.name "KazNeco"
+& $git remote set-url origin $remote
 & $git add .
 & $git status --short
 
@@ -28,7 +29,7 @@ $msg = Read-Host "Commit message (Enter for update)"
 if ($msg -eq "") { $msg = "update" }
 
 & $git commit -m $msg
-& $git push origin main
+& $git -c credential.helper="" push origin main
 
 Write-Host "=== Pushed to GitHub ==="
 Read-Host "Press Enter to close"
